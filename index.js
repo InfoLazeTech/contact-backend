4667a841e45c7135b1171080669bdb604468cc3c
InfoLazeTech <deepakinf30@gmail.com>
2025-09-12 11:44:26 +0530

    latest

--- index.js
+++ index.js
@@ -8,26 +8,17 @@ import contactRoutes from "./routes/ConatctUs.Route.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-// ✅ Configure CORS
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://your-frontend-domain.com"], // allow both local + deployed frontend
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-// ✅ Explicit OPTIONS handler (for preflight)
-app.options("*", cors());
 app.get("/", (req, res) => {
   res.send("Hello Backend");
 });
 app.use("/api", contactRoutes);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -37,3 +28,5 @@ mongoose
   .catch((err) => {
     console.log("MongoDB Connenction Error: ", err);
   });
+
+

019d5a8dfe51ea07bc3a58ea6cfab513dbcbc3d7
InfoLazeTech <deepakinf30@gmail.com>
2025-09-12 11:22:24 +0530

    checkijn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.get("/", (req, res) => {
 });
 app.use("/api", contactRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${PORT}`);
+// });
 
 mongoose
   .connect(process.env.MONGODB_URI)
